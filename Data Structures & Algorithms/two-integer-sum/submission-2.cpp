2f09c1cf32d1fa77aa77366cc742f6d0865a8ce8
sriram789 <43289824+sriram789@users.noreply.github.com>
2026-06-16 20:52:18 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> lookupTable;
+        lookupTable.reserve(nums.size());
+
+        for (int index = 0 ; index < nums.size() ; index++) {
+            int difference = target - nums[index];
+
+            if (lookupTable.find(difference) != lookupTable.end()) {
+                return vector<int>{lookupTable[difference], index};
+            } else {
+                lookupTable[nums[index]] = index;
+            }
+        }
+    }
+};
